1341ec2db4d756622e95b51c54685a9ee5888c40
Egor <cracker.s@yandex.ru>
2026-07-08 12:53:19 +0600

    debug cors

--- backend/app/core/cors.py
+++ backend/app/core/cors.py
@@ -1,15 +1,11 @@
 from fastapi.middleware.cors import CORSMiddleware
 
+
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "http://localhost:5173",
-            "http://127.0.0.1:5173",
-            "https://guntt-repo-test-aa0oq7mlt-sidorinegor-gmailcoms-projects.vercel.app",
-        ],
-        allow_credentials=True,
+        allow_origins=["*"],
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
-        expose_headers=["*"],
     )
