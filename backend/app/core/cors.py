bb3f41df6b36d3cf0fd893d006da6876838ccf70
Egor <cracker.s@yandex.ru>
2026-07-08 12:23:19 +0600

    fix cors for vercel frontend

--- backend/app/core/cors.py
+++ backend/app/core/cors.py
@@ -6,6 +6,7 @@ def setup_cors(app):
         allow_origins=[
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://guntt-repo-test-aa0oq7mlt-sidorinegor-gmailcoms-projects.vercel.app",
         ],
         allow_credentials=True,
         allow_methods=["*"],
